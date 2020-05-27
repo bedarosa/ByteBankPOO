2ac9aa0dfd278f3769301c23a3b070a4571aeab2
Bernardo Rosa Pinheiro <bedarosa99@gmail.com>
2020-05-27 02:23:49 -0300

    Projeto de POO banco

---
+++ 99-ByteBank/ContaPoupanca.cs
@@ -0,0 +1,38 @@
+﻿public class ContaPoupanca
+{
+    public string titular;
+    public int agencia;
+    public double saldo;
+
+    public bool Sacar(double valor)
+    {
+        if (valor > this.saldo)
+        {
+            return false;
+        }
+        
+        this.saldo -= valor;
+        return true;
+        
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transferir(double valor, ContaPoupanca contaDestino)
+    {
+        if (valor > this.saldo)
+        {
+            return false;
+        }
+        
+        this.saldo -= valor;
+        contaDestino.Depositar(valor);
+        return true;
+     
+    }
+
+
+}
---
+++ 99-ByteBank/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaPoupanca contaDeBernardo = new ContaPoupanca();
+            contaDeBernardo.titular = "Bernardo";
+            contaDeBernardo.agencia = 0001;
+            contaDeBernardo.saldo = 1000;
+
+            Console.WriteLine("Titular: " + contaDeBernardo.titular);
+            Console.WriteLine("Agencia: " + contaDeBernardo.agencia);
+            Console.WriteLine("Saldo: " + contaDeBernardo.saldo);
+
+            contaDeBernardo.Sacar(500);
+
+            Console.WriteLine("Novo saldo: " + contaDeBernardo.saldo);
+
+            contaDeBernardo.Depositar(200);
+
+            Console.WriteLine("Novo saldo: " + contaDeBernardo.saldo);
+
+            ContaPoupanca contaDeRafaela = new ContaPoupanca();
+
+            contaDeRafaela.titular = "Rafaela";
+            contaDeRafaela.agencia = 0002;
+            contaDeRafaela.saldo = 1000;
+
+            Console.WriteLine("Titular: " + contaDeRafaela.titular);
+            Console.WriteLine("Agencia: " + contaDeRafaela.agencia);
+            Console.WriteLine("Saldo: " + contaDeRafaela.saldo);
+
+            contaDeRafaela.Transferir(200, contaDeBernardo);
+
+            Console.WriteLine("Saldo: " + contaDeRafaela.saldo);
+            Console.WriteLine("Saldo: " + contaDeBernardo.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
